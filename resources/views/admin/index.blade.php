847db417d607f54e0f7ffc5305aea01b2037fd85
Leopold Bodden <lbodden@drivegroupllc.com>
2018-09-04 13:54:33 -0400

    minor bug fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,82 +1,81 @@
 @extends('layout')
 
 
-@section('title') Admin Dashboard :: @parent @stop
+@section('title')  Admin Dashboard :: @parent @stop
 
 @section('content')
 <div id="row-main" class="row">
-  <div id="container-main" class="container-fluid">
-    <div id="col-main" class="col-xs-12">
-      <div class="page-header">
-        <h1 id="dashboard">Admin Dashboard</h1>
-      </div>
-      <div id="admin_tab_panel" role="tabpanel">
-
-        @include('admin.partials.nav-tabs', ["adminActive"=>'Home'])
-
-        <div class="tab-content">
-          <div role="tabpanel" class="tab-pane tab-pane-admin active">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="panel panel-default">
-                  <div class="panel-heading">
-                    <h3 class="panel-title">Latest Orders</h3>
-                  </div>
-                  <table class="panel-body table table-bordered">
-                    @foreach($latest_orders as $lorder)
-                    <tr>
-                      <td>
-                        {{ $lorder->shippingname }}
-                      </td>
-                      <td>
-                        {{ $lorder->created_at->format('m-d-Y') }}
-                      </td>
-                      <td>
-                        @foreach($lorder->details as $ldetail)
-                        @if($ldetail->product)
-                        {{ $ldetail->product->category?$ldetail->product->category->name:'Uncategorized ' }} - {{ $ldetail->product->name }}<br/>
-                        @endif
-                        @endforeach
-                      </td>
-                      <td>
-                        @foreach($lorder->details as $ldetail)
-                        {{ $ldetail->quantity }}<br/>
-                        @endforeach
-                      </td>
-                      <td>
-                        ${{ \number_format($lorder->total,2) }}
-                      </td>
-                    </tr>
-                    @endforeach
-                  </table>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="panel panel-default">
-                  <div class="panel-heading">
-                    <h3 class="panel-title">Latest Products</h3>
-                  </div>
-                  <table class="panel-body table table-bordered">
-                    @foreach($latest_products as $latest)
-                    <tr>
-                      <td>
-                        <img src="{{ asset($latest->picture?'pictures/'.$latest->picture:'images/noimg.gif') }}" class="img-responsive center-block" style="max-height:40px; max-width: 100px;"/>
-                      </td>
-                      <td>
-                       {{ $latest->name }}
-                     </td>
-                     <td>
-                      {{ $latest->category?$latest->category->name:'' }}
-                    </td>
-                  </tr>
-                  @endforeach
-                </table>
-              </div>
+    <div id="container-main" class="container-fluid">
+        <div id="col-main" class="col-xs-12">
+            <div class="page-header">
+                <h1 id="dashboard">Admin Dashboard</h1>
             </div>
-          </div>
+            <div id="admin_tab_panel" role="tabpanel">
+
+                @include('admin.partials.nav-tabs',  ["adminActive"=>'Home'])
+
+                <div class="tab-content">
+                    <div role="tabpanel" class="tab-pane tab-pane-admin active">
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Latest Orders</h3>
+                                </div>
+                                <table class="panel-body table table-bordered">
+                                    @foreach($latest_orders  as $lorder)
+                                    <tr>
+                                        <td>
+                                            {{ $lorder->shippingname }}
+                                        </td>
+                                        <td>
+                                            {{ $lorder->created_at->format('m-d-Y') }}
+                                        </td>
+                                        <td>
+                                            @foreach($lorder->details as $ldetail)
+                                                @if($ldetail->product)
+                                                    {{ $ldetail->product->category?$ldetail->product->category->name:'Uncategorized ' }} - {{ $ldetail->product->name }}<br/>
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td>
+                                            @foreach($lorder->details as $ldetail)
+                                                {{ $ldetail->quantity }}<br/>
+                                            @endforeach
+                                        </td>
+                                        <td>
+                                            ${{ \number_format($lorder->total,2) }}
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Latest Products</h3>
+                                </div>
+                                <table class="panel-body table table-bordered">
+                                    @foreach($latest_products  as $latest)
+                                    <tr>
+                                        <td>
+                                            <img src="{{ asset($latest->picture?'pictures/'.$latest->picture:'images/noimg.gif') }}" class="img-responsive center-block" style="max-height:40px; max-width: 100px;"/>
+                                        </td>
+                                        <td>
+                                            {{ $latest->name }}
+                                        </td>
+                                        <td>
+                                            {{ $latest->category?$latest->category->name:'' }}
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 @stop
